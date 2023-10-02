f095cca796a8e853f5e7502b45b3cdb94cbcd91e
deva2f <dev@a2f.pt>
2023-10-02 22:30:01 +0530

    remove unused packages

--- next.config.js
+++ next.config.js
@@ -34,14 +34,13 @@ const nextConfig = {
 
       if (hasFound) break
     }
-    if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
         "react/jsx-runtime.js": "preact/compat/jsx-runtime",
         react: "preact/compat",
         "react-dom/test-utils": "preact/test-utils",
         "react-dom": "preact/compat",
       });
-    }
+    
     
     return config;
   }
